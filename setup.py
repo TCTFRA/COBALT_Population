add3734d4d5e458f220ee1dc3e50d302f756f981
TCTFRA <78137269+TCTFRA@users.noreply.github.com>
2021-01-28 15:19:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,11 +26,8 @@ def pop():
 import os
 
 import discord
-from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+
 
 client = discord.Client()
 
